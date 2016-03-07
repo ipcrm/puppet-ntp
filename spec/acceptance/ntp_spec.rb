619e993857fe14a5b637811dd366e83804b769da
James E. Jones <james.jones@puppetlabs.com>
2016-03-07 15:10:40 -0600

    update ntp test

--- spec/acceptance/ntp_spec.rb
+++ spec/acceptance/ntp_spec.rb
@@ -1,8 +1,10 @@
 require 'spec_helper_acceptance'
+
 describe 'ntp class' do
-  it "applies" do
-    agent = only_host_with_role(hosts, 'frictionless')
-    run_agent_on(agent)
+  it "applies idempotently" do
+    pp = "class { 'ntp': }"
+    apply_manifest(pp, :catch_failures => true)
+    apply_manifest(pp, :catch_changes  => true)
   end
   describe file("/etc/ntp.conf") do
     it { should be_file }
