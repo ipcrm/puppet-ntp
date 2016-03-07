e89c4d3b1b0534825ee708fb5a17789cc36b5661
James E. Jones <james.jones@puppetlabs.com>
2016-03-07 13:33:23 -0600

    update tests for ntp

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -12,24 +12,9 @@ RSpec.configure do |c|
   # Configure all nodes in nodeset
     # Install module and dependencies
     hosts.each do |host|
-      if host['roles'].include?('master')
-        # Install a master
-        agent = only_host_with_role(hosts, 'frictionless')
-        master = only_host_with_role(hosts, 'master')
         ENV["PUPPET_INSTALL_TYPE"]='pe'
         run_puppet_install_helper
         copy_module_to(host, :source => proj_root, :module_name => 'ntp')
-        on master, "echo '*' > /etc/puppetlabs/puppet/autosign.conf"
-        pp = "node \"#{agent}\" { include ntp }"
-        on master, "echo '#{pp}' >> /etc/puppetlabs/code/environments/production/manifests/site.pp"
-      else
-        # Install an agent
-        master = only_host_with_role(hosts, 'master')
-        ENV["PUPPET_INSTALL_TYPE"]='agent'
-        pp = "server = #{master}"
-        on host, "mkdir -p /etc/puppetlabs/puppet"
-        run_puppet_install_helper
-        on host, "echo '#{pp}' >> /etc/puppetlabs/puppet/puppet.conf"
       end
     end
 end
