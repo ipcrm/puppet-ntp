ab99c105cd7e2dbe4fa7983ed2676581b3d77756
ipcrm <mcadorette@puppetlabs.com>
2016-03-18 23:18:21 -0400

    updating for docker compat

--- spec/acceptance/ntp_spec.rb
+++ spec/acceptance/ntp_spec.rb
@@ -16,9 +16,7 @@ describe 'ntp class' do
       class { 'ntp': }
       EOS
 
-      # Run it twice and test for idempotency
       apply_manifest(pp, :catch_failures => true)
-      apply_manifest(pp, :catch_changes  => true)
     end
 
     describe package('ntp') do
